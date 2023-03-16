4faa6a70522a45fa09adf17f7423c865336ff83e
Pablo Misael Peloc <pmpeloc@gmail.com>
2023-03-16 00:51:20 -0300

    feat: React Router Dom and router configuration added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Layout
+import { LayoutAuth } from './layouts/LayoutAuth';
+import { LayoutAdmin } from './layouts/LayoutAdmin';
+
+// Pages
+import { Login } from './pages/auth/Login';
+import { Register } from './pages/auth/Register';
+
+// Pages admin
+import { Home } from './pages/admin/Home';
+import { Error404 } from './pages/Error404';
+import { Chat } from './pages/admin/Chat';
+
 function App() {
-  return <div className='text-3xl font-bold underline'>Hola</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth' element={<LayoutAuth />}>
+          <Route index element={<Login />} />
+          <Route path='register' element={<Register />} />
+        </Route>
+        <Route path='/' element={<LayoutAdmin />}>
+          <Route index element={<Home />} />
+          <Route path='chat' element={<Chat />} />
+        </Route>
+        <Route path='*' element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
